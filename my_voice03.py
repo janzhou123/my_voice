fffdf4f2d5971c0904d1b7c9b7453b5f072b893f
janzhou123 <zwy8831@163.com>
2017-01-05 16:59:29 +0800

    这个可以用可以识别官方DEMO，和234.WAV

--- my_voice03.py
+++ my_voice03.py
@@ -1,13 +1,12 @@
 #-*- coding: utf-8 -*-
 
 import base64, requests
-d = open('D:\\PycharmProjects\\my_voice\\123.wav', 'rb').read()
-
+d = open('D:\\PycharmProjects\\my_voice\\234.wav', 'rb').read()
 # d = open('D:\\PycharmProjects\\my_voice\\123.pcm', 'rb').read()
 data = {
     # "format": "pcm",
     "format": "wav",
-    "rate": 8000,
+    "rate": 16000,
     "channel": 1,
     "token": "24.9e2b89db72fe262284a6a783e9daefd2.2592000.1486194211.282335-9165197",
     "cuid": "fe80:e5bb:8a3f:a804:6fa%12",
@@ -18,7 +17,8 @@ result = requests.post('http://vop.baidu.com/server_api', json=data, headers={'C
 # print result.text
 data_result = result.json()
 # print data_result
-if data_result['err_msg'][0]=='success.':
+print data_result['err_msg']
+if data_result['err_msg']=='success.':
     print "语音结果：" + data_result['result'][0].encode('utf-8')
 else:
     print data_result
